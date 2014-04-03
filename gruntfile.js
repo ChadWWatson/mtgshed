1514523eb119a97c460e90cde5fc436174983ae7
Chad Watson <cwwatson@gmail.com>
2014-04-03 14:36:09 -0700

    updating grunt

--- gruntfile.js
+++ gruntfile.js
@@ -126,7 +126,7 @@ module.exports = function(grunt) {
 
     //Default task(s).
     if (process.env.NODE_ENV === 'production') {
-        grunt.registerTask('default', ['jshint', 'sass', 'csslint', 'cssmin', 'uglify', 'concurrent']);
+        grunt.registerTask('default', ['jshint', 'sass', 'csslint', 'cssmin', 'uglify']);
     } else {
         grunt.registerTask('default', ['jshint', 'sass', 'csslint', 'cssmin', 'uglify', 'concurrent']);
     }
