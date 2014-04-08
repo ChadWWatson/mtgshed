576f767749e3bed62965427f3d65cb59d74c7a36
Chad Watson <cwwatson@gmail.com>
2014-04-08 10:47:16 -0700

    Workin on Inventory Page

--- public/js/controllers/inventory.js
+++ public/js/controllers/inventory.js
@@ -4,9 +4,11 @@ angular.module('mtgshed.dashboard').controller('InventoryController', ['$scope',
     $scope.global = Global;
     $scope.isViewingList = true;
     $scope.box = {};
+    $scope.currentCard = null;
     $scope.serverCards = [];
     $scope.cardSearchValue = '';    
-    
+    $scope.isLoading = false;
+
     $scope.findOne = function() {
         Inventory.get({
             id: $stateParams.inventoryId
@@ -15,10 +17,22 @@ angular.module('mtgshed.dashboard').controller('InventoryController', ['$scope',
         });
     };
 
+    $scope.selectCard = function(card) {
+        $scope.currentCard = card;
+    };
+
+    $scope.$watch("currentCard", function(newValue, oldValue) {
+        if (newValue !== {}) {
+            console.log(newValue);
+        }
+    });
+
     $scope.$watch("cardSearchValue", function(newValue, oldValue) {
         if (newValue.length > 0) {
+            $scope.isLoading = true;
             Cards.byName({ name: newValue},function(cards) {
                 if ( newValue == $scope.cardSearchValue) {
+                    $scope.isLoading = false;
                     $scope.serverCards = cards;
                 }
             });
